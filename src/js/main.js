374ac86280e12a74d8bf80a3183914646038da5d
Dao Trung Duc <daotrungduc.it@gmail.com>
2025-09-26 01:15:44 +0700

    start

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+async function loadComponents(id, file) {
+  try {
+    const response = await fetch(`src/components/${file}`);
+    const data = await response.text();
+    document.getElementById(id).innerHTML = data;
+  } catch (e) {
+    console.error("Error" + file, e);
+  }
+}
+
+loadComponents("header", "header.html");
+loadComponents("hero", "hero.html");
