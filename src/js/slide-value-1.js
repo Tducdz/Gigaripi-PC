73179a1495455239f5a311dcba2f62b6354d3901
Dao Trung Duc <daotrungduc.it@gmail.com>
2025-10-09 14:14:40 +0700

    fix: remove interval for slide-value-1

--- src/js/slide-value-1.js
+++ src/js/slide-value-1.js
@@ -1,15 +1,30 @@
-// Hàm chờ phần tử xuất hiện
-const waitForElement = (selector, callback) => {
-  const interval = setInterval(() => {
-    const el = document.querySelector(selector);
-    if (el) {
-      clearInterval(interval); // dừng kiểm tra khi tìm thấy
-      callback(el);
-    }
-  }, 100); // kiểm tra mỗi 100ms
-};
+// // Hàm chờ phần tử xuất hiện
+// const waitForElement = (selector, callback) => {
+//   const interval = setInterval(() => {
+//     const el = document.querySelector(selector);
+//     if (el) {
+//       clearInterval(interval); // dừng kiểm tra khi tìm thấy
+//       callback(el);
+//     }
+//   }, 100); // kiểm tra mỗi 100ms
+// };
 
-waitForElement(".value-slider", (slider) => {
+// waitForElement(".value-slider", (slider) => {
+//   const btnLeft = document.querySelector(".btn-value-left");
+//   const btnRight = document.querySelector(".btn-value-right");
+
+//   btnLeft.addEventListener("click", () => {
+//     slider.scrollLeft -= slider.children[1].offsetWidth + 16;
+//   });
+
+//   btnRight.addEventListener("click", () => {
+//     slider.scrollLeft += slider.children[1].offsetWidth + 16;
+//   });
+// });
+
+document.addEventListener("components:ready", () => {
+  const slider = document.querySelector(".value-slider");
+
   const btnLeft = document.querySelector(".btn-value-left");
   const btnRight = document.querySelector(".btn-value-right");
 
